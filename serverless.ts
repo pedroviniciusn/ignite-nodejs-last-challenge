9bde91407c6e92df6beb7ba00b2476a5ef42a2cd
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-28 12:07:42 -0300

    :sparkles: feat: Criaçao de todo

--- serverless.ts
+++ serverless.ts
@@ -1,33 +1,95 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
 const serverlessConfiguration: AWS = {
-  service: 'ignite-last-challenge',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "ignite-last-challenge",
+  frameworkVersion: "3",
+  plugins: [
+    "serverless-esbuild",
+    "serverless-dynamodb-local",
+    "serverless-offline",
+  ],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: "Allow",
+            Action: ["dynamodb:*"],
+            Resource: ["*"],
+          },
+        ],
+      },
     },
   },
-  functions: { },
+  functions: {
+    createTodo: {
+      handler: "src/functions/createTodo.handler",
+      events: [
+        {
+          http: {
+            path: 'createTodo/{user_id}',
+            method: 'post',
+            cors: true,
+          },
+        },
+      ],
+    },
+  },
   package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
+    },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      dbCerticateUsersIgnite: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users_todos",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S",
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH",
+            },
+          ],
+        },
+      },
     },
   },
 };
---
+++ src/functions/createTodo.ts
@@ -0,0 +1,29 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import dayjs from "dayjs";
+import { document } from "src/utils/dynamodbClient";
+import { v4 as uuidV4 } from "uuid";
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { user_id } = event.pathParameters;
+  const { title } = JSON.parse(event.body);
+
+  await document
+    .put({
+      TableName: "users_todos",
+      Item: {
+        id: uuidV4(),
+        user_id,
+        title,
+        done: false,
+        created_at: dayjs().get("date"),
+      },
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Todo criado com sucesso!",
+    }),
+  };
+};
